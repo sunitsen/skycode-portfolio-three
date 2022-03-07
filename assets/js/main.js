ad7f913c8319125c0b77205d697b9a16913a755c
sunitsen <69672778+sunitsen@users.noreply.github.com>
2022-03-07 20:09:44 +0600

    message

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,7 +136,19 @@ $(document).ready(function() {
 
 
 
+//search
+$('.search-toggle').addClass('closed');
 
+$('.search-toggle .search-icon').click(function(e) {
+    if ($('.search-toggle').hasClass('closed')) {
+        $('.search-toggle').removeClass('closed').addClass('opened');
+        $('.search-toggle, .search-container').addClass('opened');
+        $('#search-terms').focus();
+    } else {
+        $('.search-toggle').removeClass('opened').addClass('closed');
+        $('.search-toggle, .search-container').removeClass('opened');
+    }
+});
 
 
 
